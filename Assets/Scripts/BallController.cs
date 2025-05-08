8e274569e69004bcd8b0ceceaddffe0697ac24c5
David Ruiz <deivid.ruiz93@gmail.com>
2025-05-07 21:52:59 -0300

    Anotaciones

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -45,11 +45,18 @@ public class BallController : MonoBehaviour
 
         if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Brick" || collision.gameObject.tag == "Player") 
         {
-            Debug.Log($"Contact {collision.gameObject.name} Normal: {collision.contacts[0].normal}");
+            // collision.contacts o collision.GetContact() nos devuelve un array de los puntos contra los que impacto nuestro collider
+            // La normal de un objecto es el vector hacia donde la cara su superficie apunta o mira.
+            // ej: la pared derecha devuelve como normal (-1,0,0) por que esta tiene su superficie apuntando hacia la izquierda 
+            Debug.Log($"Contact {collision.gameObject.name} Normal: {collision.GetContact(0).normal}");
+            // Vector3.Reflect nos devuelve un vector resultante tomando el vector de movimiento que creamos y la normal de la superficie.
+            // ej: si nuestro vector de movimiento es (1,1,0), es decir nos estamos moviendo en diagonal hacia la derecha, e impactamos a
+            // un ladrillo desde abajo , normal = (0,-1,0), el vector reflejado podria ser (1,-1,0), haciendo que la pelota vaya hacia abajo.
             moveVector = Vector3.Reflect(moveVector, collision.contacts[0].normal);
         }
     }
 
+    // Podria utilizarse un metodo que resetee la posicion de la pelota en lugar de destruirla.
     public void ResetBall() 
     {
         
